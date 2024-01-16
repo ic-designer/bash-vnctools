9b089e4b98f6a23a37e2906ea9774fd0c1c22066
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-16 15:17:36 -0600

    Added verbosity to the dependency fetching

--- make/deps.mk
+++ make/deps.mk
@@ -8,26 +8,29 @@ WAXWING_BRANCH := main
 # Dependencies
 override BASHARGS_REPO := $(WORKDIR_DEPS)/bash-bashargs-$(BASHARGS_VERSION)
 $(BASHARGS_REPO):
-	@echo "Loading bashargs..."
+	@echo "INFO: Fetching $@..."
 	mkdir -p $(WORKDIR_DEPS)
 	curl -sL https://github.com/ic-designer/bash-bashargs/archive/refs/tags/$(BASHARGS_VERSION).tar.gz | tar xz -C $(WORKDIR_DEPS)
 	test -d $@
+	@echo "INFO: Fetching $@ completed."
 	@echo
 
 override BOXERBIRD.MK := $(WORKDIR_DEPS)/make-boxerbird/boxerbird.mk
 $(BOXERBIRD.MK):
-	@echo "Loading Boxerbird..."
+	@echo "INFO: Fetching $@..."
 	git clone --config advice.detachedHead=false --depth 1 \
 			https://github.com/ic-designer/make-boxerbird.git --branch $(BOXERBIRD_BRANCH) \
 			$(WORKDIR_DEPS)/make-boxerbird
 	test -f $@
+	@echo "INFO: Fetching $@ completed."
 	@echo
 
 override WAXWING := $(WORKDIR_DEPS)/bash-waxwing/bin/waxwing
 $(WAXWING):
-	@echo "Loading Waxwing..."
+	@echo "INFO: Fetching $@..."
 	git clone  --config advice.detachedHead=false --depth 1 \
 			https://github.com/ic-designer/bash-waxwing.git --branch $(WAXWING_BRANCH) \
 			$(WORKDIR_DEPS)/bash-waxwing
 	test -f $@
+	@echo "INFO: Fetching $@ completed."
 	@echo
