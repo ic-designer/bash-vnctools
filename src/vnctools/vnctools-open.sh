34865d62163eb65ec5dd828f91f4d914556936a7
Jeff Fredenburg <jfreden@fnal.gov>
2024-04-12 11:18:50 -0500

    added capability for vnctools-open to automatically search for a remote port

--- src/vnctools/vnctools-open.sh
+++ src/vnctools/vnctools-open.sh
@@ -9,7 +9,7 @@ function vnctools_open::parse_args() {
     bashargs::add_required_value --username
     bashargs::add_required_value --hostname
     bashargs::add_optional_value --localport 5900
-    bashargs::add_optional_value --remoteport 5900
+    bashargs::add_optional_value --remoteport AUTO
     bashargs::add_optional_flag --realvnc
     bashargs::add_optional_flag --screenshare
     bashargs::add_optional_value --sleep 4
@@ -23,23 +23,28 @@ function vnctools_open::execute() {
         set -x
     fi
 
+    local remoteport=$(bashargs::get_arg --remoteport)
+    if [[ ${remoteport} == "AUTO" ]]; then
+        remoteport=$(vnctools_open::find_available_remote_port)
+    fi
+
     clean_up () {
         for job in `jobs -p`; do
             kill ${job}
         done
     }
-
     trap "clean_up; exit 1" INT
     trap 'echo "ERROR: $(caller)" >&2' ERR
+
     ssh  -CKf -o ConnectTimeout=2 $(bashargs::get_arg --username)@$(bashargs::get_arg --hostname) \
         "pkill -9 -f -e vnctools-x11vnc-$(bashargs::get_arg --display)"
     sleep $(bashargs::get_arg --sleep)
-    ssh  -CKf -o ConnectTimeout=2 -L $(bashargs::get_arg --localport):localhost:$(bashargs::get_arg --remoteport) \
+    ssh  -CKf -o ConnectTimeout=2 -L $(bashargs::get_arg --localport):localhost:${remoteport} \
         $(bashargs::get_arg --username)@$(bashargs::get_arg --hostname) \
         "x11vnc \
             -tag vnctools-x11vnc-$(bashargs::get_arg --display) \
             -display :$(bashargs::get_arg --display) \
-            -rfbport $(bashargs::get_arg --remoteport) -localhost \
+            -rfbport ${remoteport} -localhost \
             -noshm -usepw -forever -noxdamage -snapfb -speeds dsl \
             $(bashargs::get_arg --x11vnc)"
 
@@ -57,4 +62,31 @@ function vnctools_open::execute() {
     esac
     clean_up
     wait
+}
+
+function vnctools_open::find_available_remote_port() {
+    local netstat_report="$(ssh  -CKf -o ConnectTimeout=2 \
+            $(bashargs::get_arg --username)@$(bashargs::get_arg --hostname) \
+            netstat -tulpen4 | grep LISTEN | awk '{print $4}')"
+    local min_port=1024
+    local max_port=65535
+
+    for (( port = ${min_port}; port <= ${max_port}; port++ )); do
+        local available=true
+        for entry in ${netstat_report}; do
+            if [[ ${entry} =~ ${port} ]]; then
+                available=false
+                break 1
+            fi
+        done
+        if [[ ${available} =~ true ]]; then
+            break
+        fi
+    done
+
+    if [[ ${available} =~ false ]]; then
+        echo "ERROR: No available remote port found between ${min_port} and ${max_port}" 1>&2
+        exit 1
+    fi
+    echo ${port}
 }
--- test/vnctools/test-vnctools-open.sh
+++ test/vnctools/test-vnctools-open.sh
@@ -1,18 +1,20 @@
 function test_vnctools_open_missing_display() {
     local return_code=0
+    waxwing::monkey_patch_commands_to_record_command_name_only kill open ssh trap sleep
     $(vnctools-open --hostname=  --username= ) || return_code=1
     [[ $return_code -eq 1 ]]
 }
 
 function test_vnctools_open_missing_hostname() {
     local return_code=0
+    waxwing::monkey_patch_commands_to_record_command_name_only kill open ssh trap sleep
     $(vnctools-open --display=  --username= ) || return_code=1
     [[ $return_code -eq 1 ]]
 }
 
 function test_vnctools_open_missing_username() {
     local return_code=0
-    waxwing::monkey_patch_commands_to_record_command_name_only kill open ssh trap sleepf
+    waxwing::monkey_patch_commands_to_record_command_name_only kill open ssh trap sleep
     $(vnctools-open --display= --hostname=  ) || return_code=1
     [[ $return_code -eq 1 ]]
 }
@@ -26,12 +28,13 @@ function test_vnctools_open_with_valid_args_and_default_options() {
         --username=username
     actual=$(waxwing::read_pipe)
     expected=$(cat << EOF
+ssh -CKf -o ConnectTimeout=2 username@hostname netstat -tulpen4
 trap clean_up; exit 1 INT
 trap echo "ERROR: \$(caller)" >&2 ERR
 ssh -CKf -o ConnectTimeout=2 username@hostname pkill -9 -f -e vnctools-x11vnc-display
 sleep 4
-ssh -CKf -o ConnectTimeout=2 -L 5900:localhost:5900 username@hostname x11vnc \
--tag vnctools-x11vnc-display -display :display -rfbport 5900 -localhost -noshm -usepw \
+ssh -CKf -o ConnectTimeout=2 -L 5900:localhost:1024 username@hostname x11vnc \
+-tag vnctools-x11vnc-display -display :display -rfbport 1024 -localhost -noshm -usepw \
 -forever -noxdamage -snapfb -speeds dsl
 sleep 4
 open -W vnc://localhost:5900
@@ -50,12 +53,13 @@ function test_vnctools_open_with_valid_args_and_only_non_default_localport() {
         --localport=localport
     actual=$(waxwing::read_pipe)
     expected=$(cat << EOF
+ssh -CKf -o ConnectTimeout=2 username@hostname netstat -tulpen4
 trap
 trap
 ssh -CKf -o ConnectTimeout=2 username@hostname pkill -9 -f -e vnctools-x11vnc-display
 sleep
-ssh -CKf -o ConnectTimeout=2 -L localport:localhost:5900 username@hostname x11vnc \
--tag vnctools-x11vnc-display -display :display -rfbport 5900 -localhost -noshm -usepw \
+ssh -CKf -o ConnectTimeout=2 -L localport:localhost:1024 username@hostname x11vnc \
+-tag vnctools-x11vnc-display -display :display -rfbport 1024 -localhost -noshm -usepw \
 -forever -noxdamage -snapfb -speeds dsl
 sleep
 open -W vnc://localhost:localport
