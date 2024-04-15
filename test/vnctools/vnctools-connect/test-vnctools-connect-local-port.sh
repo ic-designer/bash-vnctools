81ac8c0f3aade6a0103f4681e5a74dc1e05be1f9
Jeff Fredenburg <jfreden@fnal.gov>
2024-04-15 08:38:41 -0500

    create vnctools-connect tests for getting local ports

---
+++ test/vnctools/vnctools-connect/test-vnctools-connect-local-port.sh
@@ -0,0 +1,61 @@
+function test_vnctools_connect_get_local_listening_port() {
+    source $(which vnctools-connect)
+    waxwing::monkey_patch_commands_to_record_command_name_and_args lsof awk
+    vnctools_connect::get_local_listening_port user host
+    local actual="$(waxwing::read_pipe)"
+    local expected="$(cat << EOF
+lsof -iTCP -sTCP:LISTEN -n -P
+awk {print \$9}
+EOF
+)"
+     [[ ${actual} == ${expected} ]]
+}
+
+function test_vnctools_connect_get_local_listening_port_min_port_available() {
+    source $(which vnctools-connect)
+    waxwing::monkey_patch_commands_to_record_command_name_and_args ssh
+    local actual=$(vnctools_connect::get_local_listening_port user host 1024 1026 "$(cat << EOF
+:1026
+:1025
+EOF
+)")
+    local expected="1024"
+     [[ ${actual} == ${expected} ]]
+}
+
+function test_vnctools_connect_get_local_listening_port_max_port_available() {
+    source $(which vnctools-connect)
+    waxwing::monkey_patch_commands_to_record_command_name_and_args ssh
+    local actual=$(vnctools_connect::get_local_listening_port user host 1024 1026 "$(cat << EOF
+:1024
+:1025
+EOF
+)")
+    local expected="1026"
+     [[ ${actual} == ${expected} ]]
+}
+
+function test_vnctools_connect_get_local_listening_port_mid_port_available() {
+    source $(which vnctools-connect)
+    waxwing::monkey_patch_commands_to_record_command_name_and_args ssh
+    local actual=$(vnctools_connect::get_local_listening_port user host 1024 1026 "$(cat << EOF
+:1024
+:1026
+EOF
+)")
+    local expected="1025"
+     [[ ${actual} == ${expected} ]]
+}
+
+function test_vnctools_connect_get_local_listening_port_no_port_available() {
+    local return_code=0
+    source $(which vnctools-connect)
+    waxwing::monkey_patch_commands_to_record_command_name_and_args ssh
+    $(vnctools_connect::get_local_listening_port user host 1024 1026 "$(cat << EOF
+:1025
+:1026
+:1024
+EOF
+)") || return_code=1
+    [[ $return_code -eq 1 ]]
+}
